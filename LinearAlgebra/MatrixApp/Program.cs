85378de759420bbe3b76c53b9baece39de20e0e2
Sanjeev Kumar Mahato <eskar.anjuvem@gmail.com>
2024-08-19 10:15:23 +0530

    First Commit : Defining Matrix Library

---
+++ LinearAlgebra/MatrixApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinearAlgebraLib.MatrixCore;
+
+namespace LinearAlgebraLib.MatrixApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the order of Matrix you want to create - ");
+            Console.Write("Rows:");
+            int matrixRows = int.Parse(Console.ReadLine());
+            Console.Write("Columns:");
+            int matrixColumns = int.Parse(Console.ReadLine());
+
+            decimal[,] inputMatrix = new decimal[matrixRows, matrixColumns];
+            for (int i = 0; i < matrixRows; i++)
+            {
+                Console.WriteLine("Enter the elements of row {0}:", i + 1);
+                for (int j = 0; j < matrixColumns; j++)
+                {
+                    inputMatrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Your Input Matrix is:");
+            for (int i = 0; i < matrixRows; i++)
+            {
+                for (int j = 0; j < matrixColumns; j++)
+                {
+                    Console.Write("{0}  ", inputMatrix[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinearAlgebra/MatrixCore/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearAlgebraLib.MatrixCore
+{
+    public class Matrix
+    {
+        private int matrixRows;
+        private int matrixColumns;
+        private double[,] data;
+        
+        public int Rows 
+        { 
+            get { return matrixRows; }
+        }
+
+        public int Columns
+        {
+            get { return matrixColumns; }
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            if (rows < 0 || columns < 0)
+                throw new ArgumentException("Matrix dimensions must be positive.");
+            this.matrixRows = rows;
+            this.matrixColumns = columns;
+
+            data = new double[rows, columns];
+        }
+
+        public double this[int row, int column]
+        {
+            get 
+            {
+                if (row < 0 || column < 0 || row >= matrixRows || column >= matrixColumns)
+                    throw new IndexOutOfRangeException("Invalix Matrix index");
+                return data[row, column];
+            }
+
+            set
+            {
+                if (row < 0 || column < 0 || row >= matrixRows || column >= matrixColumns)
+                    throw new IndexOutOfRangeException("Invalix Matrix index");
+                data[row, column] = value;
+            }
+        }
+
+
+    }
+}
